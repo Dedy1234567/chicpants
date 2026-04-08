d665e78cb562ef8ea1bb31d7c7f79186d263ee12
dedy1234567 <dedmybosbos@gmail.com>
2026-04-08 19:58:22 +0700

    delete yg tidak berfungsi

--- src/main/java/com/example/demo/Model/Category.java
+++ src/main/java/com/example/demo/Model/Category.java
@@ -2,7 +2,6 @@ package com.example.demo.Model;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -10,9 +10,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-
 
 import java.util.List;
 import java.util.Optional;
